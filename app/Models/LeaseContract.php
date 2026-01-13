0925f4ebcb0fec3630d0ffdd8877f42248ab7e7c
MarGhoste <marcoscarpiocorazon@gmail.com>
2026-01-13 09:04:48 -0500

    Crear modelo LeaseContract

--- app/Models/LeaseContract.php
+++ app/Models/LeaseContract.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class LeaseContract extends Model
 {
-    //
+    // Definimos los 5 campos de nuestra prueba técnica
+    protected $fillable = [
+        'property_address', // 1. Ubicación del Inmueble
+        'landlord_name',    // 2. Identificación del Arrendador
+        'tenant_name',      // 3. Identificación del Arrendatario
+        'monthly_rent',     // 4. Renta Mensual
+        'duration_years',   // 5. Duración del Contrato
+    ];
 }
